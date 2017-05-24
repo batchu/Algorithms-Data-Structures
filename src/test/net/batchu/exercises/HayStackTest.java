d5220f6fa626aa230c3047bd6eacc706f597c06d
batchu <batchu@batchu.net>
2017-05-24 00:08:26 -0400

    Added Test cases

--- src/test/net/batchu/exercises/HayStackTest.java
+++ src/test/net/batchu/exercises/HayStackTest.java
@@ -2,7 +2,11 @@ package net.batchu.exercises;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 public class HayStackTest {
     @Test
@@ -43,28 +47,52 @@ public class HayStackTest {
     @Test
     public void find() throws Exception {
 
+        List<Character> haystack = "abcdef".chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        Map<Character, Integer> needle = HayStack.toMap("abc");
 
-
+        assertEquals(true, HayStack.find(needle,haystack, 'a'));
     }
 
     @Test
     public void search() throws Exception {
+
+        List<Character> haystack = "abcdef".chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        Map<Character, Integer> needle = HayStack.toMap("abc");
+
+        assertEquals(true, HayStack.search(0,haystack, needle));
+
+         haystack = "abcdef".chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+         needle = HayStack.toMap("abc");
+
+        assertEquals(false, HayStack.search(5,haystack, needle));
 
     }
 
     @Test
     public void updateMap() throws Exception {
 
+        List<Character> haystack = "abcdef".chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+        Map<Character, Integer> needle = HayStack.toMap("abc");
+        HayStack.updateMap(needle, haystack, 0);
+        assertEquals(needle.get('a'), null);
+
     }
 
     @Test
     public void toMap() throws Exception {
 
+        Map<Character, Integer> map = HayStack.toMap("abcdef");
+        assertEquals(map.get('a').intValue(), 1);
+        assertEquals(map.get('k'), null);
     }
 
     @Test
     public void findIndexesOf() throws Exception {
 
+        String car = "car";
+        List<Integer> indexList = HayStack.findIndexesOf('c', car.chars().mapToObj(e -> (char) e).collect(Collectors.toList()));
+        assertEquals(1, indexList.size());
+        assertEquals(0, indexList.get(0).intValue());
     }
 
     @Test
